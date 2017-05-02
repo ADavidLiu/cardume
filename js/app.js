bb5fc56d73108f8863cd0b025927b1db1c050ed7
Andrés Liu <andresdavidliu@hotmail.com>
2017-05-02 00:34:32 -0500

    Added the modals and dropdowns

--- js/app.js
+++ js/app.js
@@ -3,12 +3,17 @@ $(document).ready(function () {
     var $overlayTrigger = $(".overlay-trigger");
     var $overlay = $(".overlay");
     var $overlayClose = $(".overlay-close");
+    var $modalBackdrop = $(".modal-backdrop");
     
     $overlayTrigger.click(function () {
         $overlay.addClass("overlay--active");
     });
     
     $overlayClose.click(function () {
+        $overlay.removeClass("overlay--active");
+    });
+    
+    $modalBackdrop.click(function () {
         $overlay.removeClass("overlay--active");
     });
     
